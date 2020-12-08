d7c476410dcf344849dca0f0eb15263f9c473b68
Kamila Kazimierska <32539643+kkazimierska@users.noreply.github.com>
2020-12-08 06:50:03 +0300

    Add files via upload

---
+++ Code/auxiliary_functions/fit_spde_full.R
@@ -0,0 +1,27 @@
+# Input
+# stk.full - full stack for the estimation 
+# res.spde - inla result of the spde model fit 
+# spde - spde model, the same as for the fit
+
+# Output 
+# prediction result of the spde fit 
+
+fit_spde_full <- function(stk.full, res.spde, spde){
+  formula.model <- count ~ Intercept + f(i, model=spde)-1
+  
+  pred.res.spde <- inla(formula.model,
+                     family = "poisson",
+                     control.family = list(link = "log"),
+                     data=inla.stack.data(stk.full), ## supply the full data
+                     E = inla.stack.data(stk.full)$E,
+                     # link=1` to compute the fitted values with the same link function
+                     # as the family specified in the model
+                     control.predictor=list(compute=TRUE, link = 1,
+                                            A=inla.stack.A(stk.full)), ## full
+                     control.mode=list(theta=res.spde$mode$theta, restart=FALSE),
+                     # use posterior mode from the spde fit 
+                     # instead of refitting, the mode for hyper-param is fixed
+                     num.threads="4:1")## use mode already found
+  return(pred.res.spde)
+}
+
